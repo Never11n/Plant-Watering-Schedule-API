b08b4c5ee6322b06fc13adb02c456d7d257c6901
Maxim <never1n@ukr.net>
2024-10-28 21:16:05 +0300

    feat: create plant model

--- plant_API/models.py
+++ plant_API/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    watering_frequency_days = models.PositiveSmallIntegerField()
+    last_watered_date = models.DateField()
+    owner = models.ForeignKey(get_user_model(), related_name="plants", on_delete=models.CASCADE)
