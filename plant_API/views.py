00f31a351996770ac038b3dbfabfe9c3e9393dfa
Maxim <never1n@ukr.net>
2024-10-28 21:54:01 +0300

    add: add retrieve method for PlantAPIView

--- plant_API/views.py
+++ plant_API/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
@@ -8,6 +9,11 @@ from plant_API.serializers import PlantSerializer
 class PlantAPIView(ViewSet):
     queryset = Plant.objects.all()
 
-    def list(self, *args, **kwargs):
+    def list(self, request, *args, **kwargs):
         serializer = PlantSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        plant = get_object_or_404(self.queryset, pk=pk)
+        serializer = PlantSerializer(plant)
         return Response(serializer.data)
